3c2de0dd0951077b9804e3d0c19e21ad86308a7f
Christopher Gibbs <cdg456@yahoo.com>
2021-02-20 21:15:47 -0500

    Added Location class. Attempted to display image of location in view, but currently broken in .NET 5.0: https://github.com/dotnet/project-system/issues/4534.

---
+++ MyFirstRPG/Engine/Models/Location.cs
@@ -0,0 +1,46 @@
+﻿/// <summary>
+/// Defines the Location class.
+/// </summary>
+
+namespace Engine.Models
+{
+    /// <summary>
+    /// Represents a location on the game map.
+    /// </summary>
+    public class Location
+    {
+        /// <summary>
+        /// Gets or sets the name of the location.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the X-coordinate of the location.
+        /// </summary>
+        public int XCoordinate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Y-coordinate of the location.
+        /// </summary>
+        public int YCoordinate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description of the location.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the file path of the image that will be displayed when the player is at this location.
+        /// </summary>
+        public string ImagePath { get; set; }
+
+        public Location(string name, int xCoordinate, int yCoordinate, string description, string imagePath)
+        {
+            this.Name = name;
+            this.XCoordinate = xCoordinate;
+            this.YCoordinate = yCoordinate;
+            this.Description = description;
+            this.ImagePath = imagePath;
+        }
+    }
+}
--- MyFirstRPG/Engine/ViewModels/GameSession.cs
+++ MyFirstRPG/Engine/ViewModels/GameSession.cs
@@ -17,11 +17,17 @@ namespace Engine.ViewModels
         public Player CurrentPlayer { get; set; }
 
         /// <summary>
+        /// Gets or sets the current <see cref="Location"/> of the game.
+        /// </summary>
+        public Location CurrentLocation { get; set; }
+
+        /// <summary>
         /// Constructs a new <see cref="GameSession"/> instance.
         /// </summary>
         public GameSession()
         {
             this.CurrentPlayer = new Player("Some Call Me Tim", "Wizard");
+            this.CurrentLocation = new Location("Home", 0, -1, "It's your house. It's quite cozy.", "/Engine;component/Images/Locations/PlayerHouse.png");
         }
     }
 }
